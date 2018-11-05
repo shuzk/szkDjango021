46150d5e0871c44f034ca1d9ce8fd481858fee90
szk_django021 <szk_django021@163.com>
2018-11-06 00:29:17 +0800

    2.1——迁移，千万不要写错字段，有点麻烦的

--- booktest/models.py
+++ booktest/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # 定义图书模型类BookInfo
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)  # 图书名称
-    bpub_date = models.IntegerField()  # 发布日期
+    bpub_date = models.DateField()  # 发布日期
     bread = models.IntegerField(default=0)  # 阅读量
     bcomment = models.IntegerField(default=0)  # 评阅量
     isDelete = models.BooleanField(default=False)  # 删除逻辑
@@ -15,6 +15,6 @@ class BookInfo(models.Model):
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)  # 英雄姓名
     hgender = models.BooleanField(default=True)  # 英雄性别
-    idDelete = models.BooleanField(default=False)  # 逻辑删除
+    isDelete = models.BooleanField(default=False)  # 逻辑删除
     hcomment = models.CharField(max_length=200)  # 英雄描述信息
     hbook = models.ForeignKey("BookInfo")  # 英雄与图书表的关系为一队多，外键定义在多的一方
